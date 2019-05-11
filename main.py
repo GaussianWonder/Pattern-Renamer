0cbe371003feba4704b0c6356f71278dc63b2191
GaussianWonder <teo.virghi@yahoo.ro>
2019-05-11 19:20:25 +0300

    Stable version

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
+import os
 
-# SETUP (argument reader, help printer, etc...)
+# SETUP (argument reader, help printer, etc...) <==========================================
 def errorParsingArguments():
     print('There were errors while parsing the arguments')
     print('Foreign argument | Incomplete argument given')
@@ -12,8 +13,8 @@ def showHelp():
     print('--white-sp | -wp    => White space is a separator\n')
     print('Pattern :  (encapsulate in \" \")     ')
     print('     % => Any string combination to be ignored')
-    print('     @ => Any number NOT to be ignored')
-    print('     & => Any string NOT to be ignored\n')
+    print('     @ => Any number NOT to be ignored (stops at character  | raw ascii)')
+    print('     & => Any string NOT to be ignored (stops at whitespace | raw ascii)\n')
     print('EG:  %_$_&_% matches XASD_123_WOW_DSAX')
     print('                      ^^   ^   ^   ^^')
     print(' $ and & will be      %    $   &   % ')
@@ -29,6 +30,7 @@ path    = ''
 pattern = ''
 whiteSeparator = True # Default must be False !! CHANGE THIS
 
+#scan arguments
 sz = len(sys.argv)
 i = 1
 while(i < sz):
@@ -45,7 +47,7 @@ while(i < sz):
         pattern = sys.argv[i]
     elif arg == '--white-sp' or arg == '-wp':
         whiteSeparator = True
-    else :
+    else:
         errorParsingArguments()
         sys.exit(0)
     
@@ -58,5 +60,125 @@ if path == '' or pattern == '':
 
 print('Using path: '    + path)
 print('Using pattern: ' + pattern)
+if whiteSeparator == True:
+    print('Using white space as a separator\n')
+else:
+    print('Ignoring white space (deleting it between @ and &)\n')
 
-# SETUP DONE => FILE RENAMER:
+# SETUP DONE => FILE RENAMER <=============================================================
+
+#check if the path is valid
+if os.path.isdir(path) == False:
+    print('Invalid PATH')
+    sys.exit(0)
+
+#check pattern
+i = 1
+sz = len(pattern)
+wellFormatted = True
+while i < sz and wellFormatted == True:
+    if pattern[i - 1] == pattern[i] and (pattern[i] == '@' or pattern[i] == '&' or pattern[i] == '%'):
+        wellFormatted = False
+    i += 1
+
+if wellFormatted == False:
+    print('Invalid PATTERN')
+    print('No adjacent special characters: %% @& &%')
+    sys.exit(0)
+
+patternsToSearch = pattern.split("%")
+
+
+#renames a string based on pattern
+def rename(string, patternArray):
+    lastIndex = 0
+    modStr = string
+
+    #browse sub patterns from pattern
+    for pattern in patternArray:
+        i = 0
+        sz = len(pattern)
+        while i < sz and pattern[i] != '@' and pattern[i] != '&':
+            i += 1
+        
+        startMatch = pattern[:i]
+        endMatch   = pattern[i + 1:]
+
+        #nothing to be matched
+        if startMatch == '' and endMatch == '':
+            continue
+
+        startIndex = modStr.find(startMatch, lastIndex)
+        endIndex   = -1
+        #didn't find a matching start
+        if startIndex == -1 or startIndex >= len(modStr):
+            continue
+
+        #search a number
+        if pattern[i] == '@':   # search a number
+            i = startIndex + len(startMatch)
+            while i < len(modStr) and modStr[i] >= '0' and modStr[i] <='9':
+                i += 1
+
+            endIndex = i - 1
+            j = 0
+            while endIndex < len(modStr) and j < len(endMatch) and modStr[endIndex] == endMatch[j]:
+                endIndex += pattern
+                j += 1
+
+            #didn't find a matching end
+            if j < len(endMatch):
+                continue
+        #search the last string ending with the desired endMatch
+        else:
+            i = startIndex + len(startMatch)
+            endIndex = i
+            if len(endMatch) == 0:
+                #no endMatch
+                if whiteSeparator == True:
+                    while endIndex < len(modStr) and modStr[endIndex] != ' ' and modStr[endIndex] != '\n':
+                        endIndex += 1
+                else:
+                    endIndex = len(modStr) - 1
+
+                if endIndex >= len(modStr):
+                    endIndex = len(modStr) - 1
+            else:
+                #search last matched
+                matchedLast = -1
+                while endIndex < len(modStr):
+                    if modStr[endIndex] == pattern[0]:
+                        j = endIndex + 1
+                        k = 1
+                        while j < len(modStr) and k < len(pattern) and modStr[j] == pattern[k]:
+                            j += 1
+                            k += 1
+                        
+                        #if found
+                        if k >= len(pattern):
+                            matchedLast = j - 1
+                    endIndex += 1
+                endIndex = matchedLast
+
+        if endIndex == -1 or endIndex >= len(modStr):
+            continue
+
+        #delete mid content
+        modStr = modStr[:startIndex] + modStr[endIndex + 1:]
+        #update lastIndex reached
+        lastIndex = startIndex
+
+    return modStr
+
+# scan directory for files and rename everything according to a pattern
+directory = os.fsencode(path)
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    filepath = os.path.join(path, filename)
+    name, extension = os.path.splitext(filename)
+
+    renamed = rename(name, patternsToSearch)
+    filepathRenamed = os.path.join(path, renamed + extension)
+    print(renamed)
+    # os.rename(filepath, 'b.kml')
+
